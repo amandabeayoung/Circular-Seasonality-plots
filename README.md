66726519d47d15fff0921a24b10872014a2ce025
Amanda Young <amandabeayoung@gmail.com>
2023-12-07 14:13:09 -0900

    small updates

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Circular-Seasonality-plots
 Code for creating a circular plot showing off how climate variables vary throughout the season. 
 
-![Toolik_climate](https://github.com/amandabeayoung/Circular-Seasonality-plots/assets/21092249/00fdfb68-aa80-4800-8d7d-30cd6e7f90d5)
+
+
+
 
 ### Four data sets are used in creating these circular plots
 * hourly - Radiation (hours of daylight)
@@ -9,4 +11,4 @@ Code for creating a circular plot showing off how climate variables vary through
 - daily - Max and Min temperatures
 - Naturalist Journal- daily observations (Aurora, Snow Present, Precipitation (Rain/snow), % cloud cover)
 
-Plot looks good, though in the original I then editited it in Foxit PDF editor. 
+Labels are added through using the package patchwork and the function inset_element. 
--- circular plot.R
+++ circular plot.R
@@ -86,64 +86,87 @@ nj<-nj_data %>%
 
 ### averged years
 p<-ggplot() +
-  geom_ribbon(data = airTavg, aes(x=doy, ymin=-50, ymax=0), fill = "light blue", alpha =0.2)+
-  #geom_line(data = airTavg, aes(x=doy, y=0), colour = "cornflowerblue", linetype = "dotted")+
-  geom_ribbon(data = airTavg,aes(x = doy,ymin = air_min,ymax = air_max), fill = "red", alpha=0.1) +
-  geom_ribbon(data = airTavg,aes(x = doy,ymin = air_min_avg,ymax = air_max_avg), fill = "red", alpha=0.5) +
-  # geom_point(data=h1_all, aes(x=doy, y=45, colour = quant1), size = 2, alpha =0.5)+
+  geom_ribbon(data = airTavg, aes(x=doy, ymin=-50, ymax=0), fill = "light blue", alpha =0.2, show.legend = FALSE)+
+  geom_line(data = airTavg, aes(x=doy, y=0), colour = "cornflowerblue", linetype = "dotted")+
+  geom_ribbon(data = airTavg,aes(x = doy,ymin = air_min,ymax = air_max), fill = "red", alpha=0.1, show.legend = FALSE) +
+  geom_ribbon(data = airTavg,aes(x = doy,ymin = air_min_avg,ymax = air_max_avg), fill = "red", alpha=0.5, show.legend = FALSE) +
+  # geom_point(data=h1_all, aes(x=doy, y=45, colour = quant1), linewidth = 2, alpha =0.5)+
   scale_color_manual(values = c('NA', "yellow"))+
   new_scale_color()+
-  geom_point(data = nj, aes(x=doy, y=42, colour = aurora), size =4, alpha=0.5)+
+  geom_point(data = nj, aes(x=doy, y=42, colour = aurora), size =4, alpha=0.5, show.legend = FALSE)+
   scale_color_manual(values = c('NA', "green", 'NA'))+
   new_scale_color()+
-  geom_point(data = nj, aes(x=doy, y=35, colour = cloud1), size = 4, alpha = 0.5)+
+  geom_point(data = nj, aes(x=doy, y=35, colour = cloud1), size = 4, alpha = 0.5, show.legend = FALSE)+
   scale_color_manual(values = c('NA', "light grey"))+
   new_scale_color()+
-  geom_point(data=nj, aes(x=doy, y=25, colour = snow2),pch=16)+
+  geom_point(data=nj, aes(x=doy, y=25, colour = snow2),pch=16, show.legend = FALSE)+
   scale_color_manual(values = c('NA', "darkslategray3"))+
   new_scale_color()+
-  geom_point(data=daily_rain, aes(x=doy, y=30, colour = rain1), size=2, stroke =0.55, alpha = 0.2)+
+  geom_point(data=daily_rain, aes(x=doy, y=30, colour = rain1), size=2, stroke =0.55, alpha = 0.2, show.legend = FALSE)+
   scale_color_manual(values = c('NA', "cornflowerblue"))+
   new_scale_color()+
-  geom_point(data=nj, aes(x=doy, y=30, colour = snowed_today), pch=8, size = 2, stroke =0.75, alpha = 0.7, na.rm = TRUE)+
+  geom_point(data=nj, aes(x=doy, y=30, colour = snowed_today), pch=8, size = 2, stroke =0.75, alpha = 0.7, na.rm = TRUE, show.legend = FALSE)+
   scale_color_manual(values = c('NA', "cadetblue2"))+
   new_scale_color()+
   geom_line(data = Pyrometer, aes(x=doy, y=50, colour = pyra4), size = 5)+
-  scale_color_viridis(option = "C")+
+  scale_color_viridis(option = "C",name="Hours of Daylight")+
   new_scale_color()+
-  geom_line(data=lake_temp, aes(x=doy, y=lake_avg), colour="aquamarine4", size=1.5)+
+  geom_line(data=lake_temp, aes(x=doy, y=lake_avg), colour="aquamarine4", size=1.5, show.legend = FALSE)+
   scale_x_continuous(
     breaks = c(15,45,76,106,137,167,198,228,259,290,320,351),
     minor_breaks = c(30,60,91,121,152,182,213,243,274,304,335,366), limits = c(0,366),labels = c("January", "February", "March", "April", "May","June","July", "August", "September", "October", "November", "December"))+
   scale_y_continuous(limits = c(-50, 50), breaks = c(-40,-20, 0, 20,50), minor_breaks = c(-40,-20, 0, 20)) +
   theme_bw()+
   ggtitle("Through the year at Toolik", subtitle = "Data from the TFS Met Station and Naturalist Journal (2007-2022)")+
+  labs(fill="Hours of Daylight")+
   theme(axis.text.x = element_text(size = 14),
+        axis.text.y = element_blank(),
         panel.grid.minor.x = element_line(colour = "grey"),
+        axis.text = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title = element_blank(),
         panel.grid.major.x = element_blank(),
         panel.grid.minor.y = element_blank(),
         panel.grid.major.y = element_blank(),
         panel.border = element_blank(),
-        plot.margin = unit(c(0,0,0,0), "lines"),
+        plot.margin = margin(c(0,0,0,0)),
         plot.title = element_text(hjust = 0.5, size=15),
-        plot.subtitle = element_text(hjust=0.5, size = 10, face = "italic"))
+        plot.subtitle = element_text(hjust=0.5, size = 10, face = "italic"),
+        legend.position=c(1.04,0.5),
+        legend.title = element_text(size=9))
 
 p2<-p+coord_curvedpolar(clip = "on")+
-  theme(axis.text.x = element_text(size = 12))
+  theme(axis.text.x = element_text(size = 11))
 
 p2
 
 p3<-ggplot()+
   scale_x_continuous(limits=c(0,100))+
   scale_y_continuous(limits=c(0,100))+
-  annotate("segment", x=15, xend=30, y=15, yend=30, colour="blue")+
+  #annotate("segment", x=20, xend=35, y=6.5, yend=27, colour="#5b5b5b", size=0.35)+
+  annotate("segment", x=0, xend=38, y=82, yend=55, colour="#5b5b5b", size=0.35)+
+  annotate("segment", x=17, xend=29, y=1, yend=15.5, colour="#5b5b5b", size=0.35)+
+  annotate("segment", x=80.5, xend=99, y=29, yend=17, colour="#5b5b5b", size=0.35)+
+  annotate("segment", x=77, xend=100, y=68, yend=83, colour="#5b5b5b", size=0.35)+
+  annotate("segment", x=72, xend=85, y=83, yend=98, colour="#5b5b5b", size=0.35)+
+  annotate("text", x = 5, y = 92, label = "Temperature Range", size=2.5, colour="#5b5b5b")+
+  annotate("text", x = 16, y = 0, label = "> 50% Cloud Cover", size=2.5, colour="#5b5b5b")+  
+  annotate("text", x = 99, y = 16, label = "Precipitation", size=2.5, colour="#5b5b5b")+
+  annotate("text", x = 99, y = 13.5, label = "Snow", size=2.5, hjust = 0, colour="#5b5b5b")+
+  annotate("text", x = 99, y = 11.5, label = "Rain", size=2.5, hjust=0, colour="#5b5b5b")+
+  annotate("text", x = 1.5, y = 89, label = "Average Air", size=2.5, hjust=0, colour="#5b5b5b")+
+  annotate("text", x = 1.5, y = 87, label = "Extreme Air", size=2.5, hjust=0, colour="#5b5b5b")+
+  annotate("text", x = 1.5, y = 85, label = "Average Lake", size=2.5, hjust=0, colour="#5b5b5b")+
+  annotate("text", x = 100, y = 86, label = "Snow \n Present", size=2.5, colour="#5b5b5b")+
+  annotate("text", x = 86, y = 99, label = "Aurora Seen", size=2.5, hjust=0, colour="#5b5b5b")+
+  annotate("point",x = 0, y = 89, shape = 15, colour = "red", alpha = 0.5, size = 3)+
+  annotate("point",x = 0, y = 87, shape = 15, colour = "red", alpha = 0.1, size = 3)+
+  annotate("point",x = 0, y = 85, shape = 15, colour = "aquamarine4", size = 3)+
+  annotate("point",x = 97.5, y = 13.5, shape = 8, colour = "cadetblue2", size = 3)+
+  annotate("point",x = 97.5, y = 11.5, colour = "cornflowerblue", size = 3)+
   theme(axis.text = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_blank(),
-        #panel.grid.minor.x = element_blank(),
-        #panel.grid.major.x = element_blank(),
-        #panel.grid.minor.y = element_blank(),
-        #panel.grid.major.y = element_blank(),
         panel.border = element_blank(),
         plot.margin = unit(c(0,0,0,0), "lines"),
         panel.background = element_rect(fill='transparent'), #transparent panel bg
@@ -154,5 +177,9 @@ p3<-ggplot()+
         legend.box.background = element_rect(fill='transparent') #transparent legend panel
   )
 
-p2+inset_element(p3)
-p2 + inset_element(p3, left = 0, bottom = 0, right = 1, top = 1)
+
+p2 + inset_element(p3, left = 0, bottom = 0, right = 1, top = 1)+
+  labs(caption = "Amanda B. Young, 2023", fontface = "italic")+
+  theme(plot.caption = element_text(face="italic"))
+  
+
